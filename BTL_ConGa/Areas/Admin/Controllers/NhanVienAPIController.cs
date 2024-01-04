cf6e8e3732b3020d01c82b4cca8f08660b11a43d
HaoNguyenQuang <hao14102002123@gmail.com>
2024-01-04 18:16:01 +0700

    first commit

---
+++ BTL_ConGa/Areas/Admin/Controllers/NhanVienAPIController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BTL_ConGa.Models;
+using Microsoft.VisualBasic;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace BTL_ConGa.Areas.Admin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NhanVienAPIController : ControllerBase
+    {
+        BtlWebContext db = new BtlWebContext();
+         
+        [HttpGet]
+        public IEnumerable<Models.NhanVien> GetAllNhanVien()
+        {
+            var nhanvien = db.NhanViens.ToList();
+            return nhanvien;
+        }
+        [HttpGet("{manhanvien}")]
+        public IEnumerable<Models.NhanVien> GetNhanVienTheoMa(string manhanvien)
+        {
+            var nhanvien = db.NhanViens.Where(x=>x.MaNhanVien==manhanvien).ToList();
+            return nhanvien;
+        }
+        [HttpPost]
+        [Route("ThemNhanVien")]
+        public bool ThemNhanVien(String MaNhanVien, String TenNhanVien, String DiaChi, DateTime NgaySinh, String Email, 
+            String SoDienThoai, String GioiTinh, string TaiKhoan, string MatKhau)
+        {
+            TaiKhoan taikhoan = new TaiKhoan();
+            taikhoan.TaiKhoan1 = TaiKhoan;
+            taikhoan.MatKhau = MatKhau;
+            taikhoan.MaLoaiTaiKhoan = "LTK01";
+            db.TaiKhoans.Add(taikhoan);
+            db.SaveChanges();
+
+            Models.NhanVien nhanvien = new Models.NhanVien();
+            nhanvien.MaNhanVien = MaNhanVien;
+            nhanvien.TenNhanVien = TenNhanVien;
+            nhanvien.DiaChi = DiaChi;
+            nhanvien.NgaySinh = NgaySinh;
+            nhanvien.Email = Email;
+            nhanvien.SoDienThoai = SoDienThoai;
+            nhanvien.GioiTinh = GioiTinh;
+            nhanvien.TaiKhoan = taikhoan.TaiKhoan1;
+
+            db.NhanViens.Add(nhanvien);
+            db.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ BTL_ConGa/Areas/Admin/Controllers/VoucherAPIController.cs
@@ -0,0 +1,84 @@
+﻿using BTL_ConGa.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_ConGa.Areas.Admin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoucherAPIController : ControllerBase
+    {
+        BtlWebContext db = new BtlWebContext();
+        [HttpGet]
+        public IEnumerable<Voucher> GetAllVoucher()
+        {
+            var voucher = db.Vouchers.ToList();
+            return voucher;
+        }
+        [HttpGet("{mavoucher}")]
+        public IEnumerable<Voucher> GetVoucherTheoMa(string mavoucher)
+        {
+            var voucher = db.Vouchers.Where(x => x.MaVoucher == mavoucher).ToList();
+            return voucher;
+        }
+        [HttpDelete]
+        public Boolean XoaVoucher(string mavoucher)
+        {
+
+            try
+            {
+                var voucher = db.Vouchers.Find(mavoucher);
+                var voucherhoadon = db.HoaDonBans.Find(mavoucher);
+                if (voucher == null || voucherhoadon!=null) { return false; }
+
+                db.Vouchers.Remove(voucher);
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+
+        }
+        [HttpPost]
+        public bool ThemVoucher(String MaVoucher, String TenVoucher, int PhanTram, int SoLuong, DateTime NgayBatDau, DateTime NgayKetThuc)
+        {
+            try
+            {
+                Voucher voucher = new Voucher();
+                voucher.MaVoucher = MaVoucher;
+                voucher.TenVoucher = TenVoucher;
+                voucher.PhanTram= PhanTram;
+                voucher.SoLuong= SoLuong;
+                voucher.NgayBatDau = NgayBatDau;
+                voucher.NgayKetThuc= NgayKetThuc;
+                db.Vouchers.AddAsync(voucher);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        [HttpPut]
+        public bool SuaVoucher(String MaVoucher, String TenVoucher, int PhanTram, int SoLuong, DateTime NgayBatDau, DateTime NgayKetThuc)
+        {
+            try
+            {
+
+                Voucher voucher = db.Vouchers.FirstOrDefault(x => x.MaVoucher == MaVoucher);
+                if (voucher == null) { return false; }     
+                //voucher.MaVoucher = MaVoucher;
+                voucher.TenVoucher = TenVoucher;
+                voucher.PhanTram = PhanTram;
+                voucher.SoLuong = SoLuong;
+                voucher.NgayBatDau = NgayBatDau;
+                voucher.NgayKetThuc = NgayKetThuc;
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+
+
+        }
+    }
+}
